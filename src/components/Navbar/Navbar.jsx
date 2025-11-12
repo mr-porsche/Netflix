c7318f4738525c8b9c9a741e1928ed07189cd0c7
MrPorsche <ilyas.sayyed@hotmail.com>
2025-11-12 19:25:35 +0530

    Initialized Netflix project phase and completed Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import './Navbar.css';
+import Logo from '../../assets/logo.png';
+import Search from '../../assets/search_icon.svg';
+import Bell from '../../assets/bell_icon.svg';
+import Profile from '../../assets/profile_img.png';
+import drop_icon from '../../assets/drop_icon.svg';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="left-nav">
+        <img src={Logo} alt='netflix-logo' />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className='right-nav'>
+        <img src={Search} alt='search-icon' className='icons' />
+        <p>Children</p>
+        <img src={Bell} alt='search-icon' className='icons' />
+        <div className='profile'>
+          <img src={Profile} alt='search-icon' className='profile-icon' />
+          <img src={drop_icon} alt='search-icon' className='dropdown_icon' />
+          <div className='dropdown'>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
