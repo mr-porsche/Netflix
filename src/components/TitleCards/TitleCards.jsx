52bc248d5cc2827773e482494f17899e9fd589ab
MrPorsche <ilyas.sayyed@hotmail.com>
2025-11-13 16:04:02 +0530

    Implemented Home page and Cards component

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,33 @@
+import './TitleCards.css';
+import cards_data from '../../assets/cards/cards_data.js';
+import { useEffect, useRef } from 'react';
+
+const TitleCards = () => {
+  const cardsRef = useRef();
+  
+  
+  const handleScroll = (e)=> {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  } ;
+  
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleScroll);
+  }, []);
+
+  return (
+    <div className='cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className='card' key={index}>
+            <img src={card.image} alt="card-image" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
-import Navbar from '../../components/Navbar/Navbar';
 import './Home.css';
+import Navbar from '../../components/Navbar/Navbar';
+import Banner from '../../assets/banner.jpg';
+import Banner_Title from '../../assets/title.png';
+import Play_Button from '../../assets/play_icon.png';
+import Info_Button from '../../assets/info_icon.png';
+import TitleCards from '../../components/TitleCards/TitleCards';
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <div className='banner'>
+        <img src={Banner} alt='banner' className='banner-img' />
+        <div className="caption">
+          <img src={Banner_Title} alt='banner-title' className='caption-img' />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
+          <div className='btns'>
+            <button className='btn'><img src={Play_Button} alt='play-icon' />Play</button>
+            <button className='btn info-btn'><img src={Info_Button} alt='info-icon' />More Info</button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
     </div>
   )
 }
